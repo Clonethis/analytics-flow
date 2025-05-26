31c5b38085ce5976535b6d34aac164d4535272ab
JB <jiribrtnik@JBs-MacBook-Air.local>
2025-05-26 23:04:35 +0200

    Vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 import ClarityAnalytics from "../components/measurement/Clarity.jsx"
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/next"
 
 
 const geistSans = Geist({
@@ -40,6 +41,7 @@ export default function RootLayout({
       </body>
       <GoogleTagManager gtmId="GTM-5H4B3ZZW" />
       <ClarityAnalytics/>
+      <Analytics/>
     </html>
   );
 }

a11752144eec2020e3263e80c197c163cefbd7a4
JB <jiribrtnik@JBs-MacBook-Air.local>
2025-05-26 13:27:44 +0200

    Cenik styling

--- app/cenik/page.tsx
+++ app/cenik/page.tsx
@@ -4,7 +4,7 @@ import { CheckCircle } from "lucide-react";
 export default function CenikPage() {
   return (
     <main className="flex-1">
-      <section id="cenik" className="w-full py-12 md:py-24 lg:py-32">
+      <section id="cenik" className="w-full py-12 md:py-24 lg:py-32 justify-center flex">
         <div className="container px-4 md:px-6">
           <div className="flex flex-col items-center justify-center space-y-4 text-center">
             <div className="space-y-2">

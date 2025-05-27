fa10519ee48a207425bec208e92541544da11e05
JB <jiribrtnik@JBs-MacBook-Air.local>
2025-05-27 19:06:40 +0200

    Firebase config created

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore, Firestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // These values will be replaced by environment variables
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "YOUR_API_KEY",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "YOUR_AUTH_DOMAIN",
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "YOUR_PROJECT_ID",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "YOUR_STORAGE_BUCKET",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "YOUR_MESSAGING_SENDER_ID",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "YOUR_APP_ID",
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID // This one is optional
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyDCcZduwJWop43rcEfUXD2sC1uRkMfMo9I",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "analytics-flow-cz.firebaseapp.com",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "analytics-flow-cz",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "analytics-flow-cz.firebasestorage.app",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "711683076253",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:711683076253:web:4a54439f10d4838b2e2474",
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "G-KLRKL7KLWV"// This one is optional
 };
 
 // Initialize Firebase
